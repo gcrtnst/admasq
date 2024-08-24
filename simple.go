8b0178aa4176048dbf837d9a422954024fb68d83
gcrtnst <52910071+gcrtnst@users.noreply.github.com>
2024-08-25 00:46:01 +0900

    Add SimpleParser

--- simple.go
+++ simple.go
@@ -1,5 +1,46 @@
 package main
 
+import (
+	"bufio"
+	"io"
+)
+
+type SimpleParser struct {
+	s *bufio.Scanner
+
+	lnum   int
+	domain string
+	err    error
+}
+
+func NewSimpleParser(r io.Reader) *SimpleParser {
+	s := bufio.NewScanner(r)
+	s.Split(bufio.ScanLines)
+	return &SimpleParser{s: s}
+}
+
+func (p *SimpleParser) Parse() bool {
+	for p.s.Scan() {
+		p.lnum++
+
+		line := p.s.Bytes()
+		domain := ParseSimpleLine(line)
+		if domain != "" {
+			p.domain = domain
+			p.err = nil
+			return true
+		}
+	}
+
+	p.domain = ""
+	p.err = p.s.Err()
+	return false
+}
+
+func (p *SimpleParser) Line() int      { return p.lnum }
+func (p *SimpleParser) Domain() string { return p.domain }
+func (p *SimpleParser) Err() error     { return p.err }
+
 func ParseSimpleLine(line []byte) string {
 	lo := 0
 	for ; lo < len(line) && (line[lo] == ' ' || line[lo] == '\t'); lo++ {
--- simple_test.go
+++ simple_test.go
@@ -1,6 +1,67 @@
 package main
 
-import "testing"
+import (
+	"errors"
+	"strings"
+	"testing"
+)
+
+func TestSimpleParserParseEmpty(t *testing.T) {
+	r := strings.NewReader("")
+	p := NewSimpleParser(r)
+	HelpSimpleParserTest(t, p, false, 0, "", nil)
+}
+
+func TestSimpleParserParseNormal(t *testing.T) {
+	r := strings.NewReader("# begin\n1.example.com\n\n2.example.com\n# end")
+	p := NewSimpleParser(r)
+	HelpSimpleParserTest(t, p, true, 2, "1.example.com", nil)
+	HelpSimpleParserTest(t, p, true, 4, "2.example.com", nil)
+	HelpSimpleParserTest(t, p, false, 5, "", nil)
+}
+
+func TestSimpleParserParseCRLF(t *testing.T) {
+	r := strings.NewReader("1.example.com\r\n2.example.com\r\n")
+	p := NewSimpleParser(r)
+	HelpSimpleParserTest(t, p, true, 1, "1.example.com", nil)
+	HelpSimpleParserTest(t, p, true, 2, "2.example.com", nil)
+	HelpSimpleParserTest(t, p, false, 2, "", nil)
+}
+
+func TestSimpleParserReadError(t *testing.T) {
+	err := errors.New("test")
+	r := &ErrorReader{Err: err}
+	p := NewSimpleParser(r)
+	HelpSimpleParserTest(t, p, false, 0, "", err)
+}
+
+func HelpSimpleParserTest(t *testing.T, p *SimpleParser, wantOK bool, wantLine int, wantDomain string, wantErr error) {
+	t.Helper()
+
+	gotOK := p.Parse()
+	if gotOK != wantOK {
+		t.Errorf("ok: expected %t, got %t", wantOK, gotOK)
+	}
+
+	gotLine := p.Line()
+	if gotLine != wantLine {
+		t.Errorf("p.Line(): expected %d, got %d", wantLine, gotLine)
+	}
+
+	gotDomain := p.Domain()
+	if gotDomain != wantDomain {
+		t.Errorf("p.Domain(): expected %q, got %q", wantDomain, gotDomain)
+	}
+
+	gotErr := p.Err()
+	if gotErr != wantErr {
+		t.Errorf("p.Err(): expected %#v, got %#v", wantErr, gotErr)
+	}
+
+	if t.Failed() {
+		t.FailNow()
+	}
+}
 
 func TestParseSimpleLine(t *testing.T) {
 	tt := []struct {
@@ -51,4 +112,12 @@ func TestParseSimpleLine(t *testing.T) {
 			t.Errorf("%q: expected %q, got %q", tc.in, tc.want, got)
 		}
 	}
+}
+
+type ErrorReader struct {
+	Err error
+}
+
+func (r *ErrorReader) Read(p []byte) (int, error) {
+	return 0, r.Err
 }
