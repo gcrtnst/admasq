e1b64728e9fe0f7834e166bc5585dea2a2fa8c70
gcrtnst <52910071+gcrtnst@users.noreply.github.com>
2024-08-25 11:38:51 +0900

    Add ResourceError

--- load.go
+++ load.go
@@ -1,5 +1,9 @@
 package main
 
+import (
+	"strconv"
+)
+
 type Loader interface {
 	Load() bool
 	Filter() Filter
@@ -9,4 +13,35 @@ type Loader interface {
 type Filter struct {
 	Exception bool
 	Domain    string
+}
+
+type ResourceError struct {
+	Name string
+	Line int
+	Err  error
+}
+
+func (e *ResourceError) Error() string {
+	b := []byte(e.Name)
+
+	if e.Line > 0 {
+		if len(b) > 0 {
+			b = append(b, ':')
+		}
+		b = strconv.AppendInt(b, int64(e.Line), 10)
+	}
+
+	w := e.Err.Error()
+	if len(w) > 0 {
+		if len(b) > 0 {
+			b = append(b, ": "...)
+		}
+		b = append(b, w...)
+	}
+
+	return string(b)
+}
+
+func (e *ResourceError) Unwrap() error {
+	return e.Err
 }
---
+++ load_test.go
@@ -0,0 +1,67 @@
+package main
+
+import (
+	"errors"
+	"testing"
+)
+
+func TestResourceErrorError(t *testing.T) {
+	tt := []struct {
+		name string
+		in   *ResourceError
+		want string
+	}{
+		{
+			name: "Empty",
+			in: &ResourceError{
+				Name: "",
+				Line: 0,
+				Err:  errors.New(""),
+			},
+			want: "",
+		},
+		{
+			name: "Name",
+			in: &ResourceError{
+				Name: "hosts.txt",
+				Line: 0,
+				Err:  errors.New(""),
+			},
+			want: "hosts.txt",
+		},
+		{
+			name: "Line",
+			in: &ResourceError{
+				Name: "",
+				Line: 52149,
+				Err:  errors.New(""),
+			},
+			want: "52149",
+		},
+		{
+			name: "Err",
+			in: &ResourceError{
+				Name: "",
+				Line: 0,
+				Err:  errors.New("some error"),
+			},
+			want: "some error",
+		},
+		{
+			name: "All",
+			in: &ResourceError{
+				Name: "hosts.txt",
+				Line: 52149,
+				Err:  errors.New("some error"),
+			},
+			want: "hosts.txt:52149: some error",
+		},
+	}
+
+	for _, tc := range tt {
+		got := tc.in.Error()
+		if got != tc.want {
+			t.Errorf("%s: expected %q, got %q", tc.name, tc.want, got)
+		}
+	}
+}
